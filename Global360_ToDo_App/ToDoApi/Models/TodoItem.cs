7b558b3be280303972d095917a2e73a9fe2c7c20
Dipesh Dadhania <dipeshdadhania4@gmail.com>
2026-02-28 16:33:58 +1100

    Add Models and DTO's

---
+++ Global360_ToDo_App/ToDoApi/Models/TodoItem.cs
@@ -0,0 +1,9 @@
+namespace TodoApi.Models;
+
+public class TodoItem
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string Title { get; set; } = string.Empty;
+    public bool IsCompleted { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
